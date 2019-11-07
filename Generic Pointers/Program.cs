c74961a3f6ade9582539a0b66a1bef1b001ec1fa
anastas.kostov <anastas.kostov@DEVTEVA31>
2019-11-07 14:17:27 +0200

    Generic Pointers

---
+++ Generic Pointers/Program.cs
@@ -0,0 +1,42 @@
+﻿
+namespace Generic_Pointers
+{
+    using System;
+    using System.Runtime.CompilerServices;
+
+    unsafe class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void WriteGenericToPtr<T>(IntPtr dest, T value, int sizeOfT) where T : struct
+        {
+            byte* bytePtr = (byte*)dest;
+
+            TypedReference valueref = __makeref(value);
+            byte* valuePtr = (byte*)*((IntPtr*)&valueref);
+
+            for (int i = 0; i < sizeOfT; ++i)
+                bytePtr[i] = valuePtr[i];
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static T ReadGenericFromPtr<T>(IntPtr source, int sizeOfT) where T : struct
+        {
+            byte* bytePtr = (byte*)source;
+
+            T result = default(T);
+            TypedReference resultRef = __makeref(result);
+            byte* resultPtr = (byte*)*((IntPtr*)&resultRef);
+
+            for (int i = 0; i < sizeOfT; ++i)
+                resultPtr[i] = bytePtr[i];
+            
+            return result;
+        }
+    }
+}
+
