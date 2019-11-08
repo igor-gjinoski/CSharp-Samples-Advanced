e854343d9eaf8e5eeccac50141ec8bbfd5e50bd0
anastas.kostov <anastas.kostov@DEVTEVA31>
2019-11-08 11:17:07 +0200

    Disposable class

---
+++ Disposable_Class/Program.cs
@@ -0,0 +1,46 @@
+﻿
+namespace Disposable_Class
+{
+    using System;
+    using System.Runtime.InteropServices;
+
+    using Microsoft.Win32.SafeHandles;
+
+    public class Disposable : IDisposable
+    {
+        private readonly IntPtr _unmanagedResource;
+        private readonly SafeHandle _managedResource;
+
+        public Disposable()
+        {
+            _unmanagedResource = Marshal.AllocHGlobal(sizeof(int));
+            _managedResource = new SafeFileHandle(new IntPtr(), true);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool isManualDisposing)
+        {
+            ReleaseUnmanagedResource(_unmanagedResource);
+            if (isManualDisposing)
+                ReleaseManagedResource(_managedResource);
+        }
+
+        private void ReleaseUnmanagedResource(IntPtr intPtr)
+        {
+            Marshal.AllocHGlobal(intPtr);
+        }
+
+        private void ReleaseManagedResource(SafeHandle safeHandle)
+        {
+            if (safeHandle != null)
+                safeHandle.Dispose();
+        }
+
+        ~Disposable() { Dispose(false); }
+    }
+}
