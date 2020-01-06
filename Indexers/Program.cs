33c6dba01be4070decceb3c361281d6013be7071
anastas.kostov <anastas.kostov@DEVTEVA31>
2020-01-06 16:27:22 +0200

    Work in progress: Indexers

---
+++ Indexers/Program.cs
@@ -0,0 +1,57 @@
+﻿
+namespace Indexers
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var collection = new CustomeCollection<int>(2);
+        }
+    }
+
+    class CustomeCollection<T>
+        where T : notnull
+    {
+        private T[] array;
+        private int lastUsedIndex = 0;
+
+        public CustomeCollection() 
+            : this(10)
+        {
+        }
+
+        public CustomeCollection(uint size)
+            => array = new T[size];
+
+
+        public T this[uint index]
+        {
+            get => index < array.Length
+                ? array[index]
+                : throw new IndexOutOfRangeException();
+
+            set => array[index] = value;
+        }
+
+
+        /* Functionality */
+        public void Add(T value)
+        {
+            if (lastUsedIndex >= array.Length)
+            {
+                T[] newArr = new T[lastUsedIndex * 2];
+                array.CopyTo(newArr, 0);
+                array = newArr;
+            }
+            array[lastUsedIndex] = value;
+            lastUsedIndex++;
+        }
+
+        /* More functionality */
+        // ...
+        // ...
+        // ...
+    }
+}
