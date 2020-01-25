4eaec5177509915e9d420f6d6f8ceb5da6eed4eb
Anastas Kosstow <anastas.kosstow@protonmail.com>
2020-01-25 14:52:10 +0200

    ToLiteral()

---
+++ EscapeSpecialCharacters/Program.cs
@@ -0,0 +1,29 @@
+﻿
+namespace EscapeSpecialCharacters
+{
+    using System.IO;
+    using System.CodeDom;
+    using System.CodeDom.Compiler;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        /// <summary>
+        /// ToLiteral
+        /// Using CodeDomProvider
+        /// escape special characters from text and convert it to escaped C# string
+        /// </summary>
+        private static string ToLiteral(string UrlSet)
+        {
+            using var writer = new StringWriter();
+            using var provider = CodeDomProvider.CreateProvider("CSharp");
+
+            provider.GenerateCodeFromExpression(new CodePrimitiveExpression(UrlSet), writer, null);
+
+            return writer.ToString();
+        }
+    }
+}
