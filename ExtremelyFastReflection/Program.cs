153dd26d60fa8f163b93d47b64991258a4ad54c6
Kosstow <anastas.kosstow@protonmail.com>
2021-02-17 21:51:13 +0200

    Fast Reflection

---
+++ ExtremelyFastReflection/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace ExtremelyFastReflection
+{
+    class Program
+    {
+        static void Main()
+        {
+            var homeController = new HomeController();
+
+            var property = homeController
+                .GetType()
+                .GetProperties()
+                .FirstOrDefault(x => x.IsDefined(typeof(DataAttribute), true));
+
+            var @delegate = PropertyHelper
+                    .MakeGetter<IDictionary<string, object>>(property);
+
+            var dictionary = @delegate(homeController);
+
+            Console.WriteLine(
+                $"Key: {dictionary.Keys.First()}, " +
+                $"Value: {dictionary.Values.First()}");
+        }
+    }
+
+
+    public class HomeController
+    {
+        [Data]
+        public Dictionary<string, object> Data { get; set; }
+
+        public HomeController()
+            => Data = new Dictionary<string, object>
+            {
+                ["left"] = "right"
+            };
+    }
+
+
+    public class DataAttribute : Attribute
+    {
+    }
+}
---
+++ ExtremelyFastReflection/PropertyHelper.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Reflection;
+using System.Collections.Concurrent;
+
+namespace ExtremelyFastReflection
+{
+    public class PropertyHelper
+    {
+        private static readonly ConcurrentDictionary<string, Delegate> _cache =
+            new ConcurrentDictionary<string, Delegate>();
+
+        private static readonly MethodInfo CallInnerDelegateMethod =
+            typeof(PropertyHelper)
+            .GetMethod(nameof(CallInnerDelegate),
+                BindingFlags.NonPublic |
+                BindingFlags.Static);
+
+
+        public static Func<object, TResult> MakeGetter<TResult>(PropertyInfo property)
+        {
+            return (Func<object, TResult>)
+                _cache.GetOrAdd(property.Name, key =>
+                {
+                    var declaringClass = property.DeclaringType;
+                    var typeOfResult = typeof(TResult);
+
+                    var getMethodDelegateType =
+                        typeof(Func<,>).MakeGenericType(declaringClass, typeOfResult);
+
+                    var getMethodDelegate = property
+                        .GetMethod
+                        .CreateDelegate(getMethodDelegateType);
+
+                    var callInnerGenericMethod =
+                        CallInnerDelegateMethod
+                        .MakeGenericMethod(declaringClass, typeOfResult)
+                        .Invoke(null, new[] { getMethodDelegate });
+
+                    return (Func<object, TResult>)callInnerGenericMethod;
+                });
+        }
+
+        private static Func<object, TResult> CallInnerDelegate<TClass, TResult>(
+            Func<TClass, TResult> @delegate)
+        {
+            return instance => @delegate((TClass)instance);
+        }
+    }
+}
