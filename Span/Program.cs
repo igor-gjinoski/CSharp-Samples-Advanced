ada1390d1be84f9c54960a910bfe3df2b9e3541f
Kosstow <anastas.kosstow@protonmail.com>
2021-07-14 13:17:02 +0300

    Span

---
+++ Span/Program.cs
@@ -0,0 +1,43 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using System;
+
+namespace Span
+{
+    class Program
+    {
+        static void Main()
+        {
+            BenchmarkRunner.Run<Benchmarks>();
+        }
+    }
+
+    
+    [MemoryDiagnoser]
+    public class Benchmarks
+    {
+        private static readonly string stringDate = "14 08 2021";
+
+        [Benchmark]
+        public (int day, int month, int year) ReadDateUsingStringSplit()
+        {
+            var day = int.Parse(stringDate.Substring(0, 2));
+            var month = int.Parse(stringDate.Substring(3, 2));
+            var year = int.Parse(stringDate.Substring(6));
+
+            return (day, month, year);
+        }
+
+        [Benchmark]
+        public (int day, int month, int year) ReadDateUsingSpan()
+        {
+            ReadOnlySpan<char> dateSpan = stringDate;
+
+            var day = int.Parse(dateSpan.Slice(0, 2));
+            var month = int.Parse(dateSpan.Slice(3, 2));
+            var year = int.Parse(dateSpan.Slice(6));
+
+            return (day, month, year);
+        }
+    }
+}
