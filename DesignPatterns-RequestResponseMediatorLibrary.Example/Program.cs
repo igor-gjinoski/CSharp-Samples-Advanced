d17bededcfff61067432247a738a515ad0fac594
Kosstow <anastas.kosstow@protonmail.com>
2021-09-20 10:41:34 +0300

    test mediator

--- DesignPatterns-RequestResponseMediatorLibrary.Example/Program.cs
+++ DesignPatterns-RequestResponseMediatorLibrary.Example/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System.Threading;
+using System.Threading.Tasks;
 using DesignPatterns_RequestResponseMediatorLibrary.Abstractions;
 using DesignPatterns_RequestResponseMediatorLibrary.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection;
@@ -9,13 +10,15 @@ namespace DesignPatterns_RequestResponseMediatorLibrary.Example
     {
         static async Task Main()
         {
+            var assemblies = typeof(Program).Assembly;
+
             var serviceProvider = new ServiceCollection()
-                .AddMediator(ServiceLifetime.Scoped, typeof(Program))
+                .AddMediator(config => config.AsSingleton(), assemblies)
                 .BuildServiceProvider();
 
             IMediator mediator = serviceProvider.GetRequiredService<IMediator>();
 
-            await mediator.SendAsync(GetRequest());
+            await mediator.SendAsync(GetRequest(), CancellationToken.None);
         }
 
         static ConsoleLogRequest GetRequest()
