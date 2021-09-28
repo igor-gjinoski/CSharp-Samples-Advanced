1c0f3b0858bfa6c8be9e123c88f0a950612d3ca8
Kosstow <anastas.kosstow@protonmail.com>
2021-09-28 14:06:27 +0300

    Regex

---
+++ Regex/Program.cs
@@ -0,0 +1,21 @@
+﻿
+using System.Text.RegularExpressions;
+
+namespace Regexes
+{
+    class Program
+    {
+        static void Main()
+        {
+        }
+        private static void UseRegexWithMatchCollection()
+        {
+            string text = "A regular expression is used to check if a string matches a pattern or not";
+            MatchCollection matchCollection = Regex.Matches(text, "[a-zA-Z0-9 ]+");
+
+            foreach (Match match in matchCollection)
+            {
+            }
+        }
+    }
+}
