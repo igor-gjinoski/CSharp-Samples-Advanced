2ff054be4ab7708b0e83ed39fa4a1979e9af45e2
Kosstow <anastas.kosstow@protonmail.com>
2021-10-24 14:23:24 +0300

    Func providers

--- DesignPatterns-Factory/Implementation/Shipping/Factories/BaseProviderFactory.cs
+++ DesignPatterns-Factory/Implementation/Shipping/Factories/BaseProviderFactory.cs
@@ -1,14 +1,32 @@
 ﻿
+using DesignPatterns_Factory.Implementation.Shipping.Utils;
+using System;
+using System.Collections.Generic;
+
 namespace DesignPatterns_Factory.Implementation.Shipping.Factories
 {
     public abstract class BaseProviderFactory
     {
+        protected IDictionary<string, Func<BaseProvider>> Providers = CreateMap();
+
         public abstract BaseProvider CreateShippingProvider(string country);
 
         public BaseProvider GetShippingProvider(string country)
         {
             var provider = CreateShippingProvider(country);
             return provider;
+        }
+
+        private static IDictionary<string, Func<BaseProvider>> CreateMap()
+        {
+            return new Dictionary<string, Func<BaseProvider>>()
+            {
+                { "Bulgaria", () => new BulgarianShippingProvider(CreateShippingCostCalculator(50)) },
+                { "Norway", () => new NorwegianShippingProvider(CreateShippingCostCalculator(100)) },
+            };
+
+            ShippingCostCalculator CreateShippingCostCalculator(decimal internationalShippingFee)
+                => new(internationalShippingFee);
         }
     }
 }
--- DesignPatterns-Factory/Implementation/Shipping/Factories/StandardShippingProviderFactory.cs
+++ DesignPatterns-Factory/Implementation/Shipping/Factories/StandardShippingProviderFactory.cs
@@ -1,5 +1,4 @@
-﻿using DesignPatterns_Factory.Implementation.Shipping.Utils;
-using System;
+﻿using System;
 
 namespace DesignPatterns_Factory.Implementation.Shipping.Factories
 {
@@ -7,24 +6,11 @@ namespace DesignPatterns_Factory.Implementation.Shipping.Factories
     {
         public override BaseProvider CreateShippingProvider(string country)
         {
-            BaseProvider shippingProvider;
-
-            if (country == "Bulgaria")
-            {
-                var costCalculator = new ShippingCostCalculator(internationalShippingFee: 50);
-                shippingProvider = new BulgarianShippingProvider(costCalculator);
-            }
-            else if (country == "Norway")
-            {
-                var costCalculator = new ShippingCostCalculator(internationalShippingFee: 100);
-                shippingProvider = new NorwegianShippingProvider(costCalculator);
-            }
-            else
-            {
+            Providers.TryGetValue(country, out var provider);
+            if (provider is null)
                 throw new NotSupportedException("No provider found!");
-            }
 
-            return shippingProvider;
+            return provider();
         }
     }
 }
