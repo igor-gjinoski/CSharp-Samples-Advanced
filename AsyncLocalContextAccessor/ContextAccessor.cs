737a4a9c8c08bded492d965385c4c17887b72eb5
Kosstow <anastas.kosstow@protonmail.com>
2021-11-01 16:53:20 +0200

    ContextAccessor impl

--- AsyncLocalContextAccessor/ContextAccessor.cs
+++ AsyncLocalContextAccessor/ContextAccessor.cs
@@ -6,5 +6,39 @@ namespace AsyncLocalContextAccessor
     public class ContextAccessor<TContext> : IContextAccessor<TContext>
         where TContext : DbContext
     {
+        private static readonly AsyncLocal<ContextHolder> _contextCurrent = new();
+
+        public TContext? Context
+        {
+            get
+            {
+                return _contextCurrent.Value?.Context;
+            }
+            set
+            {
+                var holder = _contextCurrent.Value;
+                if (holder != null)
+                {
+                    // Clear current Context trapped in the AsyncLocals, as its done.
+                    holder.Context = null;
+                }
+
+                if (value != null)
+                {
+                    // Use an object indirection to hold the HttpContext in the AsyncLocal,
+                    // so it can be cleared in all ExecutionContexts when its cleared.
+                    _contextCurrent.Value = 
+                        new() 
+                        { 
+                            Context = value 
+                        };
+                }
+            }
+        }
+
+        private class ContextHolder
+        {
+            public TContext? Context;
+        }
     }
 }
