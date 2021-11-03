ce14f8e5f24b46b0373e824aa41e8e8f65b98e1a
Kosstow <anastas.kosstow@protonmail.com>
2021-11-03 15:26:58 +0200

    operators

--- ExplicitAndImplicitOperators/Program.cs
+++ ExplicitAndImplicitOperators/Program.cs
@@ -1,10 +1,41 @@
 ﻿
 namespace ExplicitAndImplicitOperators
 {
+    /*
+        In general:
+            Implicit: something is being done for you automatically.
+            Explicit: you've written something in the source code to indicate what you want to happen.
+     */
+
     class Program
     {
         static void Main()
         {
+            RoleWithImplicitOperator implicitRole = "";
+            RoleWithExplicitOperator explicitRole = (RoleWithExplicitOperator)"";
+
         }
+    }
+
+    class RoleWithImplicitOperator
+    {
+        public string RoleName { get; set; }
+
+        public static implicit operator RoleWithImplicitOperator(string roleName)
+            => new() 
+            {
+                RoleName = roleName
+            };
+    }
+
+    class RoleWithExplicitOperator
+    {
+        public string RoleName { get; set; }
+
+        public static explicit operator RoleWithExplicitOperator(string roleName)
+            => new()
+            {
+                RoleName = roleName
+            };
     }
 }
